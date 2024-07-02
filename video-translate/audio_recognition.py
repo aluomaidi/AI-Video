0c82e1f215e4e5b91600e394c7b0da89f5247788
aluomaidi <aluomaidi419@gmail.com>
2024-07-02 21:22:55 +0800

    新增翻译和语音合成

--- video-translate/audio_recognition.py
+++ video-translate/audio_recognition.py
@@ -1,22 +1,33 @@
 import whisper
 from whisper.utils import get_writer
 import time
-
+from text_translation import  NiuTransTranslator
 # Record the start time
 start_time = time.time()
-model = whisper.load_model("medium")
+model = whisper.load_model("large")
 # Record the end time
 end_time = time.time()
 # Calculate the elapsed time
 elapsed_time = end_time - start_time
 # Print the elapsed time
-print(f"Elapsed time: {elapsed_time} seconds")
+print(f"模型加载时间: {elapsed_time} seconds")
 
 voice = "重庆森林片段.mp4"
 # voice = "20240624104058895.mp4"
 
+# Record the start time
+start_time = time.time()
+
 result = model.transcribe(voice,language="zh",word_timestamps=True,initial_prompt="请转录为简体中文。")
 print(result["text"])
+segments =result["segments"]
+translator = NiuTransTranslator()
+if segments:
+    for segment in segments:
+        recognized_text = segment['text']
+        trans_text = translator.translate(recognized_text, from_lang='zh', to_lang='en')
+        dst_text = trans_text['data']['result']['trans_result']['dst']
+        print("翻译前：" + recognized_text + "翻译后：" + dst_text)
 
 srt_writer = get_writer("srt", "./output/")
 srt_writer(result, voice, {"max_line_count":1,"max_line_width":20})
@@ -26,13 +37,4 @@ end_time = time.time()
 # Calculate the elapsed time
 elapsed_time = end_time - start_time
 # Print the elapsed time
-print(f"Elapsed time: {elapsed_time} seconds")
-
-# # 加载模型（可以选择不同的模型，如 tiny, base, small, medium, large）
-# model = WhisperModel("large")
-# # 进行语音识别
-# segments, info = model.transcribe(voice,language="zh",word_timestamps=True,initial_prompt="请转录为简体中文。")
-
-# # 输出识别结果
-# for segment in segments:
-#     print(f"开始时间: {segment.start}, 结束时间: {segment.end}, 文本: {segment.text}")
+print(f"语音识别时间: {elapsed_time} seconds")
--- video-translate/text_translation.py
+++ video-translate/text_translation.py
@@ -1,9 +1,107 @@
-from googletrans import Translator
+import requests
+import datetime
+import hashlib
+import base64
+import hmac
+import json
 
-text = "我是一个中国人，我爱我的祖国，我爱我的家人，我努力工作，照顾家人，报效祖国！"
-# 使用 Google Translate 进行翻译
-translator = Translator()
-translation = translator.translate(text, src='zh-CN', dest='en')
+class NiuTransTranslator:
+    def __init__(self, app_id="e8437c4a", api_key="35586b25a94f4b4fe61889fc307eaabf", api_secret="OGEzZTI5ZjlkNmE3OTg2ZGNlZGY1YzJl", host="ntrans.xfyun.cn"):
+        self.APPID = app_id
+        self.APIKey = api_key
+        self.Secret = api_secret
+        self.Host = host
+        self.RequestUri = "/v2/ots"
+        self.url = f"https://{self.Host}{self.RequestUri}"
+        self.Algorithm = "hmac-sha256"
 
-# 打印翻译结果
-print("翻译文本:", translation.text)
+    def hashlib_256(self, res):
+        m = hashlib.sha256(bytes(res.encode(encoding='utf-8'))).digest()
+        result = "SHA-256=" + base64.b64encode(m).decode(encoding='utf-8')
+        return result
+
+    def httpdate(self, dt):
+        weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][dt.weekday()]
+        month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+                 "Oct", "Nov", "Dec"][dt.month - 1]
+        return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (weekday, dt.day, month,
+                                                        dt.year, dt.hour, dt.minute, dt.second)
+
+    def generate_signature(self, digest):
+        signatureStr = "host: " + self.Host + "\n"
+        signatureStr += "date: " + self.Date + "\n"
+        signatureStr += self.HttpMethod + " " + self.RequestUri \
+                        + " " + self.HttpProto + "\n"
+        signatureStr += "digest: " + digest
+        signature = hmac.new(bytes(self.Secret.encode(encoding='utf-8')),
+                             bytes(signatureStr.encode(encoding='utf-8')),
+                             digestmod=hashlib.sha256).digest()
+        result = base64.b64encode(signature)
+        return result.decode(encoding='utf-8')
+
+    def init_header(self, data):
+        digest = self.hashlib_256(data)
+        sign = self.generate_signature(digest)
+        authHeader = 'api_key="%s", algorithm="%s", ' \
+                     'headers="host date request-line digest", ' \
+                     'signature="%s"' \
+                     % (self.APIKey, self.Algorithm, sign)
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Method": "POST",
+            "Host": self.Host,
+            "Date": self.Date,
+            "Digest": digest,
+            "Authorization": authHeader
+        }
+        return headers
+
+    def get_body(self, text, from_lang='auto', to_lang='auto'):
+        content = str(base64.b64encode(text.encode('utf-8')), 'utf-8')
+        postdata = {
+            "common": {"app_id": self.APPID},
+            "business": {
+                "from": from_lang,
+                "to": to_lang,
+            },
+            "data": {
+                "text": content,
+            }
+        }
+        body = json.dumps(postdata)
+        return body
+
+    def translate(self, text, from_lang='auto', to_lang='auto'):
+        self.Date = self.httpdate(datetime.datetime.utcnow())
+        self.HttpMethod = "POST"
+        self.HttpProto = "HTTP/1.1"
+
+        body = self.get_body(text, from_lang, to_lang)
+        headers = self.init_header(body)
+
+        try:
+            response = requests.post(self.url, data=body, headers=headers, timeout=8)
+            if response.status_code == 200:
+                respData = json.loads(response.text)
+                return respData
+            else:
+                print(f"HTTP请求失败，状态码：{response.status_code}，错误信息：{response.text}")
+                return None
+        except Exception as e:
+            print(f"请求异常：{e}")
+            return None
+
+if __name__ == '__main__':
+    app_id = "e8437c4a"
+    api_key = "35586b25a94f4b4fe61889fc307eaabf"
+    api_secret = "OGEzZTI5ZjlkNmE3OTg2ZGNlZGY1YzJl"
+
+    translator = NiuTransTranslator(app_id, api_key, api_secret)
+    text = "爱你,爱你。"
+    result = translator.translate(text, from_lang='zh', to_lang='en')
+    json.dumps(result) 
+    if result:
+        print(result)
+    else:
+        print("翻译失败或结果为空。")
